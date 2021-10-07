29a7ec00740c5668282fd54b7673c7ad94808a01
Sampo Siltanen <sampo.siltanen@reaktor.com>
2021-10-07 10:30:53 +0300

    Change timezones to helsinki time

--- src/messageTemplate.ts
+++ src/messageTemplate.ts
@@ -1,43 +1,47 @@
-import { Match } from './domain'
-import { DateTime } from 'luxon'
+import { Match } from "./domain"
+import { DateTime } from "luxon"
 
 const liveOrUpcomingMatchTime = (match: Match) => {
-  if (match.startsAt === null) return ''
+  if (match.startsAt === null) return ""
   else {
-    const start = DateTime.fromJSDate(match.startsAt)
+    const start = DateTime.fromJSDate(match.startsAt).setZone(
+      "Europe/Helsinki"
+    )
     const diff = start.diffNow()
-    const timestamp = start.toLocaleString(DateTime.DATETIME_FULL)
-    if (diff.valueOf() < 0) return '<b>LIVE NOW!</b>'
+    const timestamp = start.toLocaleString(DateTime.DATETIME_MED_WITH_WEEKDAY)
+    if (diff.valueOf() < 0) return "<b>LIVE NOW!</b>"
     else {
-      const dur = diff.shiftTo('days', 'hours', 'minutes').toObject()
+      const dur = diff.shiftTo("days", "hours", "minutes").toObject()
       const days = dur.days ? dur.days : 0
       const hours = dur.hours ? dur.hours : 0
       const minutes = dur.minutes ? dur.minutes : 0
-      return `<b>in ${days ? days + 'd ' : ''}${hours ? hours + 'h ' : ''}${Math.round(minutes)}min \n@ ${timestamp}</b>`
+      return `<b>in ${days ? days + "d " : ""}${
+        hours ? hours + "h " : ""
+      }${Math.round(minutes)}min \n@ ${timestamp}</b>`
     }
   }
 }
 
 const concludedMatchTime = (match: Match) => {
-  if (match.startsAt === null) return ''
+  if (match.startsAt === null) return ""
   else {
-      const timestamp = DateTime
-          .fromJSDate(match.startsAt)
-          .toLocaleString(DateTime.DATETIME_FULL)
-      return `<b>Match was played \n@ ${timestamp}</b>`
+    const timestamp = DateTime.fromJSDate(match.startsAt)
+      .setZone("Europe/Helsinki")
+      .toLocaleString(DateTime.DATETIME_MED_WITH_WEEKDAY)
+    return `<b>Match was played \n@ ${timestamp}</b>`
   }
 }
 
-const score = (match: Match) => `<b>${match.score ? match.score : 'vs'}</b>`
+const score = (match: Match) => `<b>${match.score ? match.score : "vs"}</b>`
 
 const streams = (match: Match) => {
   const strm = match.streams
-    .map(s => `<a href="${s.url}">${s.platform}</a>`)
-    .join('\n')
-  return strm.length > 0 ? `<b>Streams</b>\n${strm}` : ''
+    .map((s) => `<a href="${s.url}">${s.platform}</a>`)
+    .join("\n")
+  return strm.length > 0 ? `<b>Streams</b>\n${strm}` : ""
 }
 
-const league = (match: Match) => 
+const league = (match: Match) =>
   `<b>League</b>\n<a href="${match.league?.url}">${match.league?.name}</a>`
 
 const teamA = (match: Match) =>
@@ -45,7 +49,6 @@ const teamA = (match: Match) =>
 
 const teamB = (match: Match) =>
   `<b><a href="${match.teamB.url}">${match.teamB.name}</a></b>`
-
 
 export function matchTemplate(match: Match) {
   return `
